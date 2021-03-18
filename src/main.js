728c2157a5bb260973b43c4f890f7d936f820fe4
dstark <gorokhov.dv93@gmail.com>
2021-03-18 17:01:28 +0300

    added sass styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/sass/app.sass'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
